fa8c30bb14f8149768d7b17c94488422cbedc4d8
Roland Mackintosh <rolandmackintosh@outlook.com>
2020-11-15 21:22:24 -0800

    Add project files.

---
+++ Numbers_Next_Prime_Number/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Numbers_Next_Prime_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // finds prime numbers until the user stops asking for the next one.
+            List<int> foundPrimes = new List<int>();
+            foundPrimes.Add(1);
+            foundPrimes.Add(3);
+            foundPrimes.Add(7);
+            
+            foreach (int item in foundPrimes)
+            {
+                Console.WriteLine(item);
+            }
+
+            var p = new Program();
+            var k = p.Five(3);
+            Console.WriteLine(k);
+            Console.WriteLine(Charles("abc"));
+
+        }
+        public int Five(int x)
+        {
+            return x;
+        }
+
+        static string Charles(string charlie)
+        {
+            return charlie + charlie;
+        }
+    }
+}
