d47e57f0e7cc144b7edfc2f907cc4243877f2872
Roland Mackintosh <rolandmackintosh@outlook.com>
2020-11-15 22:24:46 -0800

    program gets to user prompt, then stops?

--- Numbers_Next_Prime_Number/Program.cs
+++ Numbers_Next_Prime_Number/Program.cs
@@ -14,13 +14,37 @@ namespace Numbers_Next_Prime_Number
             // a prime is a number that is only divisible by itself. 
             // so all the numbers smaller than it mustyield floats.
             // *** *** *** ***
+            var tester = new Program();
+
             var userWantsToContinue = true;
             while (userWantsToContinue)
             {
-                var locatedPrime = 10;
+                int largestPrimeSoFar;
+                if (foundPrimes.Count == 0) // for initial condition, where we haven't found any primes yet
+                {
+                    largestPrimeSoFar = 0;
+                } else
+                {
+                    largestPrimeSoFar = foundPrimes[foundPrimes.Count - 1];
+                }
+                // start looking for primes larger than largestPrimeSoFar
+                var lookingForNextPrime = true;
+                var currentNumberToCheck = largestPrimeSoFar;
+                while (lookingForNextPrime)
+                {
+                    currentNumberToCheck++;
+                    // the magic happens...
+                    var numberIsPrime = tester.CheckIfPrime(currentNumberToCheck);
+
+                    if (numberIsPrime)
+                    {
+                        break;
+                    }
+                }
+                var locatedPrime = currentNumberToCheck;
                 foundPrimes.Add(locatedPrime);
-                // the magic happens...
-                Console.WriteLine(locatedPrime);
+                // output the magic
+                Console.WriteLine(locatedPrime); // expect 1, 2, 3, 5, 7, 11, 13, 17...
 
                 // prime found, prompt user, "do u want next prime found?"
                 Console.WriteLine("Find the next prime?");
@@ -28,25 +52,24 @@ namespace Numbers_Next_Prime_Number
                 if (feedback == "n" || feedback == "no" || feedback == "N" || feedback == "No")
                 {
                     userWantsToContinue = false;
-                } else
+                } 
+            }
+        }
+
+        public bool CheckIfPrime(int numberToCheck)
+        {
+            var divisor = 1;
+            while (divisor < numberToCheck)
+            {
+                double isWholeNumber = (double)numberToCheck / (double)divisor;
+                bool divisibleNumberFound = isWholeNumber == Math.Floor(isWholeNumber);
+
+                if (divisibleNumberFound)
                 {
-                    continue;
+                    return false;
                 }
             }
-            //foundPrimes.Add(1);
-            //foundPrimes.Add(3);
-            //foundPrimes.Add(7);
-
-            //foreach (int item in foundPrimes)
-            //{
-            //    Console.WriteLine(item);
-            //}
-
-            //var p = new Program();
-            //var k = p.Five(3);
-            //Console.WriteLine(k);
-            //Console.WriteLine(Charles("abc"));
-
+            return true;
         }
         public int Five(int x)
         {
